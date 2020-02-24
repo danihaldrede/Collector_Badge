cd1121df65ecb427b5616152a857457e527cf7db
danihaldrede <59898921+danihaldrede@users.noreply.github.com>
2020-02-24 11:00:28 -0500

    Create animals_array.js

---
+++ animals_array.js
@@ -0,0 +1,33 @@
+// initialize array with data 
+
+let animals = [ 'cows', 'horses', 'chickens'];
+
+// add element to array 
+animals.push('chickens'); 
+
+animals.unshift('donkeys'); 
+
+animals.unshift('pony');
+
+animals.push('hen');
+
+// elements order practice
+
+console.log(animals[1]);
+
+console.log(animals[2]); 
+
+console.log(animals[3]);
+
+console.log(animals[4]);
+
+console.log(animals[5]); 
+
+
+console.log(first); 
+
+animals.reverse();
+
+console.log(last); 
+
+animals.sort();
